90a8612903350a11249389245b19666e2c637950
marnixah <marnix@hagehoeve.com>
2021-08-30 20:02:28 +0200

    feat: moved output to generate_waybar

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,13 @@ fn main() {
     if month == 8 {
       now = now + Duration::days(365 * 10 + 2);
     }
-    let waybar = generate_waybar(
+    generate_waybar(
       &now.format("%b %e %Y").to_string(),
       "monkey",
       &now.format("%b %-d, %H:%M:%S").to_string(),
       "calendar",
       23,
     );
-    println!("{}", waybar.to_string());
 
     sleep(time::Duration::from_millis(1000));
   }
@@ -31,12 +30,13 @@ fn generate_waybar(
   tooltip: &str,
   class: &str,
   percentage: u8,
-) -> serde_json::Value {
-  return json!({
+) {
+  let waybar = json!({
     "text": text,
     "alt": alt,
     "tooltip": tooltip,
     "class": class,
     "percentage": percentage
   });
+  println!("{}", waybar.to_string());
 }
