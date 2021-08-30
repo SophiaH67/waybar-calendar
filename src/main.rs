d231c8f22ced1cf2b823ef08d856527959714a19
marnixah <marnix@hagehoeve.com>
2021-08-30 20:00:13 +0200

    feat: implemented basic date

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 use serde_json::json;
 
+use std::thread::sleep;
+use std::time;
+
+use chrono::{DateTime, Duration, Utc};
+
 fn main() {
-  let waybar = generate_waybar("test", "monkey", "tool", "yeah", 23);
-  println!("{}", waybar.to_string());
+  loop {
+    let mut now: DateTime<Utc> = Utc::now();
+    let month = now.format("%m").to_string().parse::<i32>().unwrap();
+    if month == 8 {
+      now = now + Duration::days(365 * 10 + 2);
+    }
+    let waybar = generate_waybar(
+      &now.format("%b %e %Y").to_string(),
+      "monkey",
+      &now.format("%b %-d, %H:%M:%S").to_string(),
+      "calendar",
+      23,
+    );
+    println!("{}", waybar.to_string());
+
+    sleep(time::Duration::from_millis(1000));
+  }
 }
 
-fn generate_waybar(text: &str, alt: &str, tooltip: &str, class: &str, percentage: u8) -> serde_json::Value {
+fn generate_waybar(
+  text: &str,
+  alt: &str,
+  tooltip: &str,
+  class: &str,
+  percentage: u8,
+) -> serde_json::Value {
   return json!({
     "text": text,
     "alt": alt,
